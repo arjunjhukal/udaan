8416c8d455dec689272468c9168a4f19645aa898
arjun <arjunjhukal.makura@gmail.com>
2026-04-01 10:47:28 +0545

    enabled the screen protection

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import Toast from "./components/organism/Toast/index.tsx";
 import GlobalRoutes from "./routes/Routes.tsx";
 import { store } from "./store/store.ts";
 import UdaanThemeProvider from "./ThemeProvider.tsx";
+import ScreenProtection from "./ScreenProtection.tsx";
 i18n
 	.use(HttpApi)
 	.use(LanguageDetector)
@@ -39,11 +40,11 @@ createRoot(document.getElementById("root")!).render(
 			<I18nextProvider i18n={i18n}>
 				<Suspense fallback={<div>Loading...</div>}>
 					<UdaanThemeProvider>
-						{/* <ScreenProtection> */}
+						<ScreenProtection>
 						<GlobalRoutes />
 						<Toast />
 						<PreviewPDF />
-						{/* </ScreenProtection> */}
+						</ScreenProtection>
 					</UdaanThemeProvider>
 				</Suspense>
 			</I18nextProvider>
